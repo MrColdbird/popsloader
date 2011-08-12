103ab40c915c96ed5b6ca17a03c1dbb33f3a8916
Hrimfaxi <outmatch@gmail.com>
2011-08-12 23:58:16 +0800

    popcorn completes all model patching on 6.60

--- popsloader/popcorn/popcorn_patch_offset.c
+++ popsloader/popcorn/popcorn_patch_offset.c
@@ -37,35 +37,35 @@ PatchOffset g_660_offsets = {
 		.decomp = {
 			{ 0x000D5424, 0x0000DB78 }, // 01G
 			{ 0x000D64DC, 0x0000DB78 }, // 02G
-			{ 0xDEADBEEF, 0xDEADBEEF }, // 03G
-			{ 0xDEADBEEF, 0xDEADBEEF }, // 04G
-			{ 0xDEADBEEF, 0xDEADBEEF }, // 05G
+			{ 0x000D64DC, 0x0000DB78 }, // 03G
+			{ 0x000D654C, 0x0000DBE8 }, // 04G
+			{ 0x000D84D8, 0x0000E300 }, // 05G
 			{ 0xDEADBEEF, 0xDEADBEEF }, // unused
-			{ 0xDEADBEEF, 0xDEADBEEF }, // 07G
+			{ 0x000D656C, 0x0000DBE8 }, // 07G
 			{ 0xDEADBEEF, 0xDEADBEEF }, // unused
-			{ 0xDEADBEEF, 0xDEADBEEF }, // 09G
+			{ 0x000D656C, 0x0000DBE8 }, // 09G
 		},
 		.ICON0SizeOffset = {
 			0x00036D50, // 01G
 			0x00037D8C, // 02G
-			0xDEADBEEF, // 03G
-			0xDEADBEEF, // 04G
-			0xDEADBEEF, // 05G
+			0x00037D8C, // 03G
+			0x00037E04, // 04G
+			0x00039BEC, // 05G
 			0xDEADBEEF, // unused
-			0xDEADBEEF, // 07G
+			0x00037E1C, // 07G
 			0xDEADBEEF, // unused
-			0xDEADBEEF, // 09G
+			0x00037E1C, // 09G
 		},
 		.manualNameCheck = {
 			0x00025248, // 01G
 			0x00025754, // 02G
-			0xDEADBEEF, // 03G
-			0xDEADBEEF, // 04G
-			0xDEADBEEF, // 05G
+			0x00025754, // 03G
+			0x000257CC, // 04G
+			0x00026060, // 05G
 			0xDEADBEEF, // unused
-			0xDEADBEEF, // 07G
+			0x000257E4, // 07G
 			0xDEADBEEF, // unused
-			0xDEADBEEF, // 09G
+			0x000257E4, // 09G
 		},
 		.sceMeAudio_67CD7972_NID = 0x2AB4FE43,
 	},
